461f227959b369b21a650f0cadcdb329a232ece9
TheGam1ngWizard <55005148+TheGam1ngWizard@users.noreply.github.com>
2021-03-27 13:18:40 -0400

    Update Event.java

--- Event.java
+++ Event.java
@@ -2,10 +2,10 @@ public class Event {
     private int EventSequenceNumber;
     private int EventDataValue;
 
-    public Event Event(int seqNum, int data){
+    public Event(int seqNum, int data){
         EventSequenceNumber = seqNum;
         EventDataValue = data;
-        return this;
+        return;
     }
     public int getEventSequenceNumber(){
         return EventSequenceNumber;
