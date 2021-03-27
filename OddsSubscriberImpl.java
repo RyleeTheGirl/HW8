f2f9e218c0f49cfe8849a2d4f26c3a6a0d6f8dd2
TheGam1ngWizard <55005148+TheGam1ngWizard@users.noreply.github.com>
2021-03-27 12:44:01 -0400

    Update OddsSubscriberImpl.java

--- OddsSubscriberImpl.java
+++ OddsSubscriberImpl.java
@@ -1,10 +1,18 @@
 public class OddsSubscriberImpl implements SubscriberInterface{
-	private int eventsReceived;
-	private int threshold;
-	private PublisherImpl publisherImpl = new PublisherImpl();
-	
-	@Override
-	public void notifySubscriber() {
-		
-	}
+    private int eventsReceived = 0;
+    private int threshold = 21;
+    private PublisherImpl publisherImpl = new PublisherImpl();
+
+    public void notifySubscriber(Event theEvent) {
+        if(theEvent.getEventDataValue()%2 != 0) {
+            eventsReceived = eventsReceived + 1;
+            if(eventsReceived <= threshold) {
+                System.out.println("SubscriberOdds:0 Event is odd: " + theEvent.getEventDataValue());
+
+            }
+            if(eventsReceived == threshold) {
+                publisherImpl.unregisterSubscriber(this);
+            }
+        }
+    }
 }
