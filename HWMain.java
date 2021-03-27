09dedea79b61efcf35018716afa76a01974c2197
RyleeTheGirl <42972486+RyleeTheGirl@users.noreply.github.com>
2021-03-27 13:05:09 -0400

    Update HWMain.java

--- HWMain.java
+++ HWMain.java
@@ -1,2 +1,14 @@
 public class HWMain {
+    
+    PublisherImpl publisher = new PublisherImpl();
+    EventGenerator eventGenerator = new EventGenerator();
+    OddsSubscriberImpl oddsSubscriber = new OddsSubscriberImpl();
+    ThreesSubscriverImpl threesSubscriver = new ThreesSubscriverImpl();
+    EvensSubscriberImpl evensSubscriber = new EvensSubscriberImpl();
+
+    publisher.register(oddSubscriver);
+    
+
+
 }
+
