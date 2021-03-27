d06ddaa63d221b8ec6d52d7addf351be4849f6b6
RyleeTheGirl <42972486+RyleeTheGirl@users.noreply.github.com>
2021-03-27 12:30:56 -0400

    Update EventGenerator.java

--- EventGenerator.java
+++ EventGenerator.java
@@ -1,8 +1,32 @@
+import java.util.List;
+import java.lang.*;
+import java.util.Random;
+
 public class EventGenerator {
     private List<SubscriberInterface> myList;
 
-    public EventGenerator EventGenerator(){
-        return this;
+    public EventGenerator() {}
+
+    public void runSimulation(){
+        Random random = new Random();
+        for (int i = 0; i < 200; i++){
+            System.out.println("Sequence Number: " + i);
+            int data = random.nextInt(200);
+            System.out.println("Data: " + data);
+            Event theNewEvent = generateEvent(i, data);
+            for(int k =0; k<myList.size(); k++) {
+                myList.get(k).notifySubscriber(theNewEvent);
+            }
+            if (theNewEvent.getEventDataValue()%40 == 0) {
+                for(int j = 0; j < myList.size(); j++) {
+                    myList.get(j).registerSubscriber(myList.get(j));
+                }
+            }
+        }
+    }
+
+    private Event generateEvent(int i, int data) {
+        return new Event(i, data);
     }
 
 
