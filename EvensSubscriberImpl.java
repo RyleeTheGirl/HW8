a2cd4aade5dad6c896ca032a29facac3a5c7d28b
RyleeTheGirl <42972486+RyleeTheGirl@users.noreply.github.com>
2021-03-27 12:40:21 -0400

    Update EvensSubscriberImpl.java

--- EvensSubscriberImpl.java
+++ EvensSubscriberImpl.java
@@ -1,2 +1,18 @@
 public class EvensSubscriberImpl implements SubscriberInterface{
+    private int eventsReceived = 0;
+    private int threshold = 14;
+    private PublisherImpl publisherImpl = new PublisherImpl();
+
+    public void notifySubscriber(Event theEvent) {
+        if(theEvent.getEventDataValue()%2 == 0) {
+            eventsReceived = eventsReceived + 1;
+            if(eventsReceived <= threshold) {
+                System.out.println("SubscriberEvens: Event is even: " + theEvent.getEventDataValue());
+
+            }
+            if(eventsReceived == threshold) {
+                publisherImpl.unregisterSubscriber(this);
+            }
+        }
+    }
 }
