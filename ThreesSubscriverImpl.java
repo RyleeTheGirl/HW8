d0f301f37a1793034ec2d0a2ead722d5f4b6cc5f
TheGam1ngWizard <55005148+TheGam1ngWizard@users.noreply.github.com>
2021-03-27 12:47:01 -0400

    Update ThreesSubscriverImpl.java

--- ThreesSubscriverImpl.java
+++ ThreesSubscriverImpl.java
@@ -1,2 +1,18 @@
 public class ThreesSubscriverImpl implements SubscriberInterface{
+    private int eventsReceived = 0;
+    private int threshold = 10;
+    private PublisherImpl publisherImpl = new PublisherImpl();
+
+    public void notifySubscriber(Event theEvent) {
+        if(theEvent.getEventDataValue()%3 == 0) {
+            eventsReceived = eventsReceived + 1;
+            if(eventsReceived <= threshold) {
+                System.out.println("SubscriberThrees Event is divisible by 3: " + theEvent.getEventDataValue());
+
+            }
+            if(eventsReceived == threshold) {
+                publisherImpl.unregisterSubscriber(this);
+            }
+        }
+    }
 }
