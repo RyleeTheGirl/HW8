3dd955210a843e82de79bcfe7fbcb452709d0144
RyleeTheGirl <42972486+RyleeTheGirl@users.noreply.github.com>
2021-03-27 12:48:22 -0400

    Update PublisherImpl.java

--- PublisherImpl.java
+++ PublisherImpl.java
@@ -1,6 +1,19 @@
-public class PublisherImpl implements PublisherInterface{
-    @Override
-    public void notifySubscribers() {
+import java.util.List;
 
+public class PublisherImpl implements PublisherInterface{
+    private List<SubscriberInterface> myList;
+    @Override
+    public void notifySubscribers(Event theEvent) {
+
+    }
+    public void registerSubscriber(SubscriberInterface s){}
+
+    @Override
+    public void unregisterSubscriber(SubscriberInterface s) {
+
+    }
+
+    public PublisherImpl PublisherImpl(){
+        return this;
     }
 }
