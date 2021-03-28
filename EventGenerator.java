a722097f4b9b979de32e3085998c3b17abd19db4
RyleeTheGirl <42972486+RyleeTheGirl@users.noreply.github.com>
2021-03-28 16:43:07 -0400

    Update EventGenerator.java

--- EventGenerator.java
+++ EventGenerator.java
@@ -1,27 +1,30 @@
+package HW8;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.lang.*;
 import java.util.Random;
 
 public class EventGenerator {
-    private List<SubscriberInterface> myList;
+    //private List<SubscriberInterface> myList = new ArrayList<>();
 
     public EventGenerator() {}
 
-    public void runSimulation(){
+    public void runSimulation(PublisherImpl publisher){
         Random random = new Random();
         for (int i = 0; i < 200; i++){
             System.out.println("Sequence Number: " + i);
             int data = random.nextInt(200);
             System.out.println("Data: " + data);
             Event theNewEvent = generateEvent(i, data);
-            for(int k =0; k<myList.size(); k++) {
-                myList.get(k).notifySubscriber(theNewEvent);
-            }
             if (theNewEvent.getEventDataValue()%40 == 0) {
-                for(int j = 0; j < myList.size(); j++) {
-                    myList.get(j).getPublisherImpl().registerSubscriber(myList.get(j));
-                }
-                
+                System.out.println("Re-registering all subscribers");
+                publisher.reregisterSubscribers();
+
             }
+            publisher.notifySubscribers(theNewEvent);
+
+
         }
     }
 
