4d877eca6b929c8ac2f394f2596344eda0eadc1d
RyleeTheGirl <42972486+RyleeTheGirl@users.noreply.github.com>
2021-03-28 16:42:49 -0400

    Update Event.java

--- Event.java
+++ Event.java
@@ -1,11 +1,12 @@
+package HW8;
+
 public class Event {
     private int EventSequenceNumber;
     private int EventDataValue;
 
-    public Event(int seqNum, int data){
+   Event(int seqNum, int data){
         EventSequenceNumber = seqNum;
         EventDataValue = data;
-        return;
     }
     public int getEventSequenceNumber(){
         return EventSequenceNumber;
