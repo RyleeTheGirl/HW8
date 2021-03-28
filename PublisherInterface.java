5614a0594597a9373d2aef1f371fc26e1303f844
RyleeTheGirl <42972486+RyleeTheGirl@users.noreply.github.com>
2021-03-28 16:44:53 -0400

    Update PublisherInterface.java

--- PublisherInterface.java
+++ PublisherInterface.java
@@ -1,5 +1,12 @@
-public interface PublisherInterface {
+package HW8;
+
+
+
+public abstract interface PublisherInterface {
+
     public void notifySubscribers(Event theEvent);
     public void registerSubscriber(SubscriberInterface s);
+    public void reregisterSubscribers();
     public void unregisterSubscriber(SubscriberInterface s);
 }
+
