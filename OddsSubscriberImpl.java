66d44834ce435799654e579012e5554d4bcdb415
RyleeTheGirl <42972486+RyleeTheGirl@users.noreply.github.com>
2021-03-28 16:43:54 -0400

    Update OddsSubscriberImpl.java

--- OddsSubscriberImpl.java
+++ OddsSubscriberImpl.java
@@ -1,20 +1,30 @@
+package HW8;
+
 public class OddsSubscriberImpl implements SubscriberInterface{
     private int eventsReceived = 0;
     private int threshold = 21;
     private PublisherImpl publisherImpl = new PublisherImpl();
 
-    public void notifySubscriber(Event theEvent) {
+    public boolean notifySubscriber(Event theEvent) {
         if(theEvent.getEventDataValue()%2 != 0) {
             eventsReceived = eventsReceived + 1;
             if(eventsReceived <= threshold) {
-                System.out.println("SubscriberOdds:0 Event is odd: " + theEvent.getEventDataValue());
+                System.out.println("SubscriberOdds: HW8.Event is odd: " + theEvent.getEventDataValue());
 
             }
-            if(eventsReceived == threshold) {
+            if(eventsReceived >= threshold) {
+                System.out.println("Odd threshold reached: " + threshold);
                 publisherImpl.unregisterSubscriber(this);
+                return false;
             }
         }
+        return true;
     }
+
+    public void setEventsReceived(int eventsReceived) {
+        this.eventsReceived = eventsReceived;
+    }
+
     public PublisherImpl getPublisherImpl() {
         return publisherImpl;
     }
