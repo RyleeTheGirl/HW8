e779203ecc72398c0fa097d50cabfd584af1caa3
RyleeTheGirl <42972486+RyleeTheGirl@users.noreply.github.com>
2021-03-28 16:43:32 -0400

    Update HWMain.java

--- HWMain.java
+++ HWMain.java
@@ -1,14 +1,18 @@
-public class HWMain {
-    
-    PublisherImpl publisher = new PublisherImpl();
-    EventGenerator eventGenerator = new EventGenerator();
-    OddsSubscriberImpl oddsSubscriber = new OddsSubscriberImpl();
-    ThreesSubscriverImpl threesSubscriver = new ThreesSubscriverImpl();
-    EvensSubscriberImpl evensSubscriber = new EvensSubscriberImpl();
+package HW8;
 
-    publisher.register(oddSubscriver);
-    
+public class HWMain {
+    public static void main(String[] args) {
+        PublisherImpl publisher = new PublisherImpl();
+        EventGenerator eventGenerator = new EventGenerator();
+        OddsSubscriberImpl oddsSubscriber = new OddsSubscriberImpl();
+        ThreesSubscriverImpl threesSubscriver = new ThreesSubscriverImpl();
+        EvensSubscriberImpl evensSubscriber = new EvensSubscriberImpl();
+        publisher.registerSubscriber(oddsSubscriber);
+        publisher.registerSubscriber(threesSubscriver);
+        publisher.registerSubscriber(evensSubscriber);
+
+        eventGenerator.runSimulation(publisher);
+    }
 
 
 }
-
