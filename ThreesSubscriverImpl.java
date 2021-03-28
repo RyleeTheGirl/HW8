c82854435408b140c04232bdfa8246b503b156af
RyleeTheGirl <42972486+RyleeTheGirl@users.noreply.github.com>
2021-03-28 16:45:27 -0400

    Update ThreesSubscriverImpl.java

--- ThreesSubscriverImpl.java
+++ ThreesSubscriverImpl.java
@@ -1,19 +1,33 @@
+package HW8;
+
 public class ThreesSubscriverImpl implements SubscriberInterface{
-    private int eventsReceived = 0;
-    private int threshold = 10;
+    private int eventsReceived;
+    private int threshold;
     private PublisherImpl publisherImpl = new PublisherImpl();
 
-    public void notifySubscriber(Event theEvent) {
+    public ThreesSubscriverImpl() {
+        eventsReceived = 0;
+        threshold = 10;
+    }
+
+    public boolean notifySubscriber(Event theEvent) {
         if(theEvent.getEventDataValue()%3 == 0) {
             eventsReceived = eventsReceived + 1;
             if(eventsReceived <= threshold) {
-                System.out.println("SubscriberThrees Event is divisible by 3: " + theEvent.getEventDataValue());
+                System.out.println("SubscriberThrees HW8.Event is divisible by 3: " + theEvent.getEventDataValue());
 
             }
-            if(eventsReceived == threshold) {
+            if(eventsReceived >= threshold) {
+                System.out.println("Threes threshold reached: " + threshold);
                 publisherImpl.unregisterSubscriber(this);
+                return false;
             }
         }
+       return true;
+    }
+
+    public void setEventsReceived(int eventsReceived) {
+        this.eventsReceived = eventsReceived;
     }
 
     public PublisherImpl getPublisherImpl() {
